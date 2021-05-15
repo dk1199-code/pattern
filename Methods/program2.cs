2dfd3a3d9263022283c5701ac3797de1553aff10
dk1199-code <83574948+dk1199-code@users.noreply.github.com>
2021-05-15 13:49:22 +0530

    Add files via upload

---
+++ Methods/program2.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace class2
+{
+    public class deepak
+    {
+         public int a=100,b=20;
+
+         public virtual void add1()
+        {
+            int c=a+b;
+            Console.WriteLine(c);
+
+        }
+
+               public deepak()
+        {
+            Console.WriteLine("Default Constructor");
+        }
+
+       
+        
+    }
+    
+}
